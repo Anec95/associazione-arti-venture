ded09ca2d98eeb2c96a5d38aaf15782cbe606eaa
Anec95 <miori.thomas95@gmail.com>
2023-05-26 13:15:41 +0200

    deploy fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
 import Home from "./Pages/Home/Home";
-import { BrowserRouter as Router } from "react-router-dom"
-import { Route, Routes } from "react-router";
-import Layout from "Pages/Layout/Layout";
 
 function App() {
     return (
